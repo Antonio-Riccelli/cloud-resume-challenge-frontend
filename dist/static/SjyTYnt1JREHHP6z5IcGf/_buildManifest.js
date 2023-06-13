54c6df6912076e20a74eea6935daae8c7144635b
Antonio Riccelli <anton.riccelli@gmail.com>
2023-06-13 14:56:41 +0100

    add styling

---
+++ dist/static/SjyTYnt1JREHHP6z5IcGf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e6c316681be4a132.css","static/chunks/pages/index-6503ec85fce23371.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
